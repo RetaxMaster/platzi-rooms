d5bcbb38990880a398cb5d003bcd8a390a2dd958
RetaxMaster <retaxmaster@hotmail.com>
2020-11-02 21:54:13 -0600

    Adicion de las paginas de creacion de salas y todo el modulo de Vuex

--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import sourceData from "./data.json";
+import countObjectProperties from "./utils";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    ...sourceData,
     user: null,
+    authId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1',
     modals: {
       login: false,
       register: false,
@@ -17,6 +21,14 @@ export default new Vuex.Store({
       state.modals[name] = value;
     },
 
+    SET_ROOM(state, { newRoom, roomId }) {
+      Vue.set(state.rooms, roomId, newRoom);
+    },
+
+    APPEND_ROOM_TO_USER(state, { roomId, userId }) {
+      Vue.set(state.users[userId].rooms, roomId, roomId);
+    },
+
   },
   actions: {
 
@@ -24,8 +36,23 @@ export default new Vuex.Store({
       commit('SET_MODAL_STATE', { name, value });
     },
 
+    CREATE_ROOM: ({state, commit}, room) => {
+      
+      const newRoom = room;
+      const roomId = `room${Math.random()}`;
+      newRoom[".key"] = roomId;
+      newRoom.userId = state.authId;
+
+      commit("SET_ROOM", { newRoom, roomId });
+      commit("APPEND_ROOM_TO_USER", { roomId, userId: newRoom.userId })
+
+    }
+
   },
   getters: {
     modals: state => state.modals,
+    authUser: state => state.users[state.authId],
+    rooms: state => state.rooms,
+    userRoomsCount: state => id => countObjectProperties(state.users[id].rooms),
   },
 });
