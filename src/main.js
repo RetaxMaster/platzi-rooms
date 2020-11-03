05ff6db0b55443963fbebf9bcd0a31a752621883
RetaxMaster <retaxmaster@hotmail.com>
2020-11-03 16:52:00 -0600

    Modificacion del data.json para insercion en firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue';
+import firebase from "firebase"; // <- Debe ir antes de App.vue
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArHNu83OhTaSJ6brHwGCwHQkSjCN-zqDo",
+  authDomain: "platzi-rooms-f1fa2.firebaseapp.com",
+  databaseURL: "https://platzi-rooms-f1fa2.firebaseio.com",
+  projectId: "platzi-rooms-f1fa2",
+  storageBucket: "platzi-rooms-f1fa2.appspot.com",
+  messagingSenderId: "381627814940",
+  appId: "1:381627814940:web:2638b781f8b3e3dd687bd9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
